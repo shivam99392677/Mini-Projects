0999636839d9e367bbbc5c701a5f7155ea9f683d
Shivam kumar <shivamkumarbaletha@gmail.com>
2025-06-05 23:40:23 +0530

    added dustbin

--- to-do/app.js
+++ to-do/app.js
@@ -1,23 +1,52 @@
-let btn = document.querySelector(".btn");
+let btn = document.querySelector(".btn-success");
 let input = document.querySelector(".form-control");
-let ul = document.querySelector("ul");
+let ul = document.querySelector(".list-group");
 
 btn.addEventListener("click",()=>{
     if(input.value != ""){
-        let li = document.createElement("li");
-        let para = document.createElement("p");
+       
         let cancelBtn = document.createElement("button");
 
+        addTask(input.value,ul);
+        input.value = "";
+        
+        let para = document.querySelector("p");
 
-        para.innerText = input.value;
-        li.appendChild(para);
-        ul.prepend(li);
         cancelBtn.classList.add("btn");
         cancelBtn.classList.add("btn-danger");
         cancelBtn.innerText="Mark as Done";
+
         para.appendChild(cancelBtn);
+        
     }else{
         alert("Please enter any task to add.")
     }
     
+})
+//function to add task to given paths
+function addTask(task,path){
+     let li = document.createElement("li");
+        let para = document.createElement("p");
+
+        para.innerText = task;
+        li.appendChild(para);
+        path.prepend(li);
+       
+}
+
+document.addEventListener("click",(event)=>{
+    if(event.target.classList.contains("btn-danger")){
+        let taskItem = event.target.parentElement;//select the <p> element
+        let bin = document.querySelector(".bin");// reference to the deleted ul
+
+        event.target.remove();//remove delete btn
+        taskItem.parentElement.remove();
+
+        let li = document.createElement("li");
+
+        li.appendChild(taskItem);
+
+        bin.prepend(li);// now move the <p>
+
+    }
 })
