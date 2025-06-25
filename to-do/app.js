39fbd8358be6fd50a13a07e3f2c9d70480f845b4
Shivam kumar <shivamkumarbaletha@gmail.com>
2025-06-25 23:17:34 +0530

    modified functionality

--- to-do/app.js
+++ to-do/app.js
@@ -2,51 +2,63 @@ let btn = document.querySelector(".btn-success");
 let input = document.querySelector(".form-control");
 let ul = document.querySelector(".list-group");
 
-btn.addEventListener("click",()=>{
-    if(input.value != ""){
-       
-        let cancelBtn = document.createElement("button");
+btn.addEventListener("click", () => {
+  if (input.value != "") {
+    let cancelBtn = document.createElement("button");
 
-        addTask(input.value,ul);
-        input.value = "";
-        
-        let para = document.querySelector("p");
+    addTask(input.value, ul);
+  } else {
+    alert("Please enter any task to add.");
+  }
+});
 
-        cancelBtn.classList.add("btn");
-        cancelBtn.classList.add("btn-danger");
-        cancelBtn.innerText="Mark as Done";
+// Adds a new task with checkbox to the given list
+function addTask(task, path) {
+  let li = document.createElement("li");
+  let para = document.createElement("p");
+  let check = document.createElement("input");
+  let i = document.createElement("i");
 
-        para.appendChild(cancelBtn);
-        
-    }else{
-        alert("Please enter any task to add.")
-    }
-    
-})
-//function to add task to given paths
-function addTask(task,path){
-     let li = document.createElement("li");
-        let para = document.createElement("p");
+  check.type = "checkbox";
+  check.classList.add("form-check-input");
 
-        para.innerText = task;
-        li.appendChild(para);
-        path.prepend(li);
-       
+  i.classList.add("fa-duotone", "fa-solid", "fa-trash");
+
+  para.innerText = task;
+
+  li.appendChild(check);
+  li.appendChild(para);
+  li.appendChild(i);
+  input.value = "";
+  path.prepend(li);
+  // Adds task at the top of the list
 }
 
-document.addEventListener("click",(event)=>{
-    if(event.target.classList.contains("btn-danger")){
-        let taskItem = event.target.parentElement;//select the <p> element
-        let bin = document.querySelector(".bin");// reference to the deleted ul
+// Moves the task to the 'bin' list when 'Mark as Done' is clicked
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("fa-duotone")) {
+    let confirmation = confirm("Do you want to Delete task?");
+    if (confirmation) {
+      let taskItem = event.target.parentElement;
+      let bin = document.querySelector(".bin"); // Target list for completed tasks
 
-        event.target.remove();//remove delete btn
-        taskItem.parentElement.remove();
+      event.target.remove(); // Removes 'Mark as Done' button
+      taskItem.remove(); // Removes the task from the current list
 
-        let li = document.createElement("li");
+      let li = document.createElement("li");
+      li.appendChild(taskItem);
 
-        li.appendChild(taskItem);
-
-        bin.prepend(li);// now move the <p>
-
+      bin.prepend(li); // Moves completed task to top of bin list
     }
-})
+  }
+});
+
+//press enter key to add task
+
+document
+  .getElementById("floating-input")
+  .addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      document.querySelector(".btn-success").click();
+    }
+  });
